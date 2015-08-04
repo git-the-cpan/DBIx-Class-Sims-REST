8f600110e15f81acdccdc4ed800859e18b87b3bc
Rob Kinyon <rob.kinyon@gmail.com>
2015-08-04 14:02:12 +0000

    Import of RKINYON/DBIx-Class-Sims-REST-0.000011.tar.gz from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Sims-REST
    gitpan-cpan-version:      0.000011
    gitpan-cpan-path:         RKINYON/DBIx-Class-Sims-REST-0.000011.tar.gz
    gitpan-cpan-author:       RKINYON
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/Sims/REST.pm
+++ lib/DBIx/Class/Sims/REST.pm
@@ -5,7 +5,7 @@ use 5.010_000;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = '0.000010';
+our $VERSION = '0.000011';
 
 use DBI;
 use Hash::Merge;
@@ -201,10 +201,16 @@ In your REST API class:
 
 In a rest.cgi file (somewhere):
 
-  use My::Sims::REST;
-  use Web::Simple 'My::Sims::REST';
+  use Module::Runtime qw(use_module);
+  my $app = use_module('My::Sims::REST')->to_psgi_app;
 
-  My::Sims::REST->run_if_script;
+  use Plack::Builder;
+
+  builder {
+    # Or whatever other middleware you want.
+    enable "Runtime"; # Adds the X-Runtime header
+    $app
+  }
 
 Then later:
 
